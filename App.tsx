734844c54440748d0bed98a407c509b924d0491a
Sebastiano Guerriero <se.guerriero@gmail.com>
2026-09-10 10:29:22 +0200

    Refactor App component structure, add Inter font support, and define color and text size variables in Tailwind CSS.

--- App.tsx
+++ App.tsx
@@ -4,9 +4,7 @@
 export default function App() {
   return (
     <main className="flex items-center justify-center min-h-screen font-primary">
-      <div data-name="frame" className="relative size-135 bg-[#000] isolate overflow-hidden">
-        {/* Build your component here in the video */}
-      </div>
+      
     </main>
   );
 }
