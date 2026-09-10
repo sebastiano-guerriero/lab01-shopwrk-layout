8c43936c2074c05b0ab74bd6f0828e7c853f713a
Sebastiano Guerriero <se.guerriero@gmail.com>
2026-09-10 10:44:57 +0200

    Update App component layout with sidebar and main container, and enhance Tailwind CSS with variable font and color settings.

--- App.tsx
+++ App.tsx
@@ -3,8 +3,15 @@
 
 export default function App() {
   return (
-    <main className="flex items-center justify-center min-h-screen font-primary">
-      
+    <main className="light h-screen p-2">
+      <div className="grid grid-cols-[256px_1fr] h-full">
+        {/* sidebar */}
+        <aside></aside>
+
+        {/* main container */}
+        <div className="bg-background-dark rounded-2xl h-full p-4 overflow-auto light:shadow-[0px_1px_0px_rgba(0,0,0,0.04),0px_0px_0px_1px_rgba(0,0,0,0.05),0px_38px_15px_-8px_rgba(0,0,0,0.01),0px_21px_13px_-4px_rgba(0,0,0,0.02),0px_9px_9px_-2px_rgba(0,0,0,0.03),0px_2px_5px_-1px_rgba(0,0,0,0.04),inset_0px_0px_0px_1px_rgba(255,255,255,0.15),inset_0px_1px_0px_rgba(255,255,255,0.7)] dark:shadow-[0px_1px_0px_rgba(0,0,0,0.04),0px_0px_0px_1px_rgba(0,0,0,0.05),0px_38px_15px_-8px_rgba(0,0,0,0.01),0px_21px_13px_-4px_rgba(0,0,0,0.02),0px_9px_9px_-2px_rgba(0,0,0,0.03),0px_2px_5px_-1px_rgba(0,0,0,0.04),inset_0px_0px_0px_1px_rgba(255,255,255,0.07),inset_0px_1px_0px_rgba(255,255,255,0.08)]">
+        </div>
+      </div>
     </main>
   );
 }
